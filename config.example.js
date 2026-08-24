031485dbb8b0b9154547b25871530d74db535b7e
nadyakunina36-maker <nadyakunina36@gmail.com>
2026-08-24 11:37:47 +0800

    Add secure Supabase sync and ECP display

--- config.example.js
+++ config.example.js
@@ -1,4 +1,4 @@
-window.NV_CONFIG = {
+window.NV_CONFIG = window.NV_CONFIG || {
   supabaseUrl: "",
   supabasePublishableKey: ""
 };
---
+++ config.js
@@ -0,0 +1,4 @@
+window.NV_CONFIG = {
+  supabaseUrl: 'https://avogakiemtwsgzknkcgy.supabase.co',
+  supabasePublishableKey: 'sb_publishable_EEMHW50Nbr3yYxJ-m4hUbA_iGDw5cLj'
+};
